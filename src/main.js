56f9e08789a6a9809ea581d33ccf0ba39b15a0c8
KrystynaShevchenko <kristinasheva1996@gmail.com>
2024-02-22 15:44:40 +0300

    fixed

--- src/main.js
+++ src/main.js
@@ -1,84 +1,36 @@
+'use strict';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const form = document.querySelector('.form');
-const gallery = document.querySelector('.gallery');
-const container = document.querySelector('div');
-const searchInput = document.querySelector('input');
+import { fetchImages } from './js/pixabay-api';
+import { galleryEl } from './js/render-functions';
+import { imageTemplate } from './js/render-functions';
 
-const showLoader = () => {
-  if (!document.querySelector('.loader')) {
-    const loader = document.createElement('span');
-    loader.classList.add('loader');
-    container.append(loader);
-  }
-};
+const formEl = document.querySelector('.img-search-form');
+const loaderEl = document.querySelector('.loader');
 
-const hideLoader = () => {
-  const loader = document.querySelector('.loader');
-  if (loader) {
-    loader.remove();
-  }
-};
+formEl.addEventListener('submit', onFormSubmit);
 
-form.addEventListener('submit', event => {
-  showLoader();
-  gallery.innerHTML = '';
-  event.preventDefault();
-  const searchTerm = searchInput.value;
-  searchImages(searchTerm);
-});
-
-function searchImages(searchTerm) {
-  const apiKey = '41764579-b97d65b31c0abd4efd9d4830e';
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      } else {
-        return response.json();
-      }
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching <br>your search query. Please try again!</br>',
-          position: 'center',
-          transitionIn: 'fadeInLeft',
-        });
-        hideLoader();
-      } else {
-        const markup = data.hits
-          .map(data => {
-            return `
-            <li class="gallery-item"><a href="${data.largeImageURL}">
-          <img class="gallery-image" src="${data.webformatURL}" alt="${data.tags}"></a>
-          <p><b>Likes: </b>${data.likes}</p>
-          <p><b>Views: </b>${data.views}</p>
-          <p><b>Comments: </b>${data.comments}</p>
-          <p><b>Downloads: </b>${data.downloads}</p>
-          </li>`;
-          })
-          .join('');
-
-        gallery.insertAdjacentHTML('beforeend', markup);
-        const lightbox = new SimpleLightbox('.gallery a', {
-          captions: true,
-          captionType: 'attr',
-          captionsData: 'alt',
-          captionPosition: 'bottom',
-          fadeSpeed: 150,
-          captionSelector: 'img',
-          captionDelay: 250,
-        });
-
-        lightbox.refresh(); // Виклик методу refresh поза обробником подій
-        hideLoader();
-      }
-    })
-    .catch(error => console.log(error));
+function onFormSubmit(e) {
+  e.preventDefault();
+  galleryEl.innerHTML = '';
+  loaderEl.classList.remove('hidden');
+  const query = e.target.elements.query.value.trim();
+  fetchImages(query).then(data => {
+    if (data.totalHits === 0 || query === '') {
+      loaderEl.classList.add('hidden');
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        messageColor: 'white',
+        backgroundColor: 'red',
+        position: 'topRight',
+      });
+    } else {
+      loaderEl.classList.add('hidden');
+      imageTemplate(data);
+    }
+  });
+  e.target.reset();
 }
